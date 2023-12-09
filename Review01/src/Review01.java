2bb7f98fafd26f7a67c422a2b09e0b9cf371d141
shingostarmeteora@yahoo.co.jp <shingostar@shore.ocn.ne.jp>
2023-12-09 22:18:46 +0900

    Review01.javaを作成しました。課題「税込価格を計算しよう」の再提出します。

--- Review01/src/Review01.java
+++ Review01/src/Review01.java
@@ -1,18 +1,13 @@
-import java.util.Scanner;
-
 public class Review01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("税抜金額を入力してください: ");
-        int price = sc.nextInt();
-        System.out.print("消費税率を入力してください: ");
-        double taxRate = sc.nextDouble();
-        double tax = price * taxRate / 100;
+        int price = 1500;
+        double taxRate = 10;
+        double tax = tax(price,taxRate);
         double taxIncludedPrice = price + tax;
         System.out.println(price+"円の商品の"+"税込金額は "+(int)taxIncludedPrice+"円(消費税は" + (int)tax+"円)です。");
     }
 
     public static double tax(int price,double taxRate) {
-        return taxRate * taxRate / 100;
+        return price * taxRate / 100;
     }
 }
